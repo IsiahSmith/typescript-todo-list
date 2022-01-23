e408d60b2c06ec362518ae1f38b370d997bf2cb2
Isiah Smith <isiahthomassmith@gmail.com>
2022-01-22 21:01:24 -0600

    local states for inputs setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React, {FC} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
 
 const App: FC = () => {
+
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodo] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.name === "task") {
+      setTask(event.target.value)
+    } else {
+      setDeadline(Number(event.target.value))
+    }
+  };
+
   return (
     <div className="App">
       <div className="Header">
         <div className="inputContainer">
-          <input type="text" placeholder="Task" />
-          <input type="number" placeholder="Deadline" />
+          <input 
+            type="text" 
+            placeholder="Task" 
+            name="task"
+            onChange={handleChange}
+          />
+          <input 
+            type="number" 
+            placeholder="Deadline"
+            name="deadline"
+          />
         </div>
         <button>Add Task</button>
       </div>
