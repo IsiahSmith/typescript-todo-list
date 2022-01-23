28d0152facf440d8bcc259704bd0e6c7b4fd4a0f
Isiah Smith <isiahthomassmith@gmail.com>
2022-01-22 21:29:34 -0600

    interfaces.ts made for newTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
+import {ITask} from './interfaces';
 
 const App: FC = () => {
 
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
-  const [todo, setTodo] = useState([]);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") {
       setTask(event.target.value)
     } else {
       setDeadline(Number(event.target.value))
     }
+  };
+
+  const addTask = (): void => {
+    const newTask = {taskName: task, deadline: deadline};
+    setTodoList([...todoList, newTask]);
+    console.log(todoList);
   };
 
   return (
@@ -27,11 +34,12 @@ const App: FC = () => {
           />
           <input 
             type="number" 
-            placeholder="Deadline"
+            placeholder="Deadline (days)"
             name="deadline"
+            onChange={handleChange}
           />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className="todoList"></div>
     </div>
